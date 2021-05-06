2bb47b8c18c130205257cff1cd25a4b56f391875
Nick Hirzel <nhirzel1@gmail.com>
2021-05-06 15:54:25 -0400

    calibrated linear advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40.2, -3.37, -2.08 }
+#define NOZZLE_TO_PROBE_OFFSET { -40.2, -3.37, -2.09 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
